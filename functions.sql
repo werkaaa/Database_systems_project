2a84003a15b0b8ca032ef3a7dbed6afa0c2ae4fa
wiktor <wiktor.tarsa@gmail.com>
2020-01-06 22:07:48 +0100

    minor changes

--- functions.sql
+++ functions.sql
@@ -11,5 +11,11 @@ create function dbo.get_average_registered_number()
 returns float
 as
 begin
-    return (select avg(registered_id) from registered group by customer_id)
+    return (select avg(r.registered_id)
+    from registered r
+    inner join conference_day_attendees cda on r.registered_id = cda.registered_id
+    inner join conference_day_reservations cdr on cda.reservation_day_id = cdr.reservation_day_id
+    inner join reservations r2 on cdr.reservation_id = r2.reservation_id
+    inner join customers c on r2.customer_id = c.customer_id
+    group by c.customer_id)
 end

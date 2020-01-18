bcd05b8c82514f1868887a3e1b39e0cd68d806d8
wiktor <wiktor.tarsa@gmail.com>
2020-01-18 20:36:11 +0100

    table functions fix

--- table_functions.sql
+++ table_functions.sql
@@ -44,7 +44,7 @@ if (object_id('dbo.conference_days_of_reservation') is not null)
 create function dbo.conference_attendees_details(@conference_id int)
 returns table
 as
-    return (select r.registered_id, r.first_name, r.last_name, r.email_address, cda.is_student
+    return (select distinct r.registered_id, r.first_name, r.last_name, r.email_address, cda.is_student
            from conference_days cd
            inner join conference_day_reservations cdr on cd.conference_day_id = cdr.conference_day_id
            inner join reservations res on res.reservation_id = cdr.reservation_id
@@ -56,7 +56,7 @@ go
 create function dbo.conference_day_attendees_details(@conference_day_id int)
 returns table
 as
-    return (select r.registered_id, r.first_name, r.last_name, r.email_address, cda.is_student
+    return (select distinct r.registered_id, r.first_name, r.last_name, r.email_address, cda.is_student
            from conference_days cd
            inner join conference_day_reservations cdr on cd.conference_day_id = cdr.conference_day_id
            inner join reservations res on res.reservation_id = cdr.reservation_id
@@ -68,7 +68,7 @@ go
 create function dbo.workshop_attendees_details(@workshop_id int)
 returns table
 as
-    return (select r.registered_id, r.first_name, r.last_name, r.email_address, cda.is_student
+    return (select distinct r.registered_id, r.first_name, r.last_name, r.email_address, cda.is_student
             from workshops w
             inner join workshop_reservations wr on w.workshop_id = wr.workshop_id
             inner join conference_day_reservations cdr on wr.reservation_day_id = cdr.reservation_day_id
@@ -81,49 +81,49 @@ go
 create function dbo.conferences_on_the_exact_day(@date date)
 returns table
 as
-    return (select * from dbo.conferences_data
+    return (select distinct * from dbo.conferences_data
            where start_date <= @date and end_date >=@date)
 go
 
 create function dbo.conferences_in_time_frame(@start_date date, @end_date date)
 returns table
 as
-    return (select * from dbo.conferences_data
+    return (select distinct * from dbo.conferences_data
            where start_date >= @start_date and end_date <=@end_date)
 go
 
 create function dbo.student_conference_attendees(@conference_id int)
 returns table
 as
-    return (select * from conference_attendees_details(@conference_id) as cad
+    return (select distinct * from conference_attendees_details(@conference_id) as cad
            where cad.is_student = 1)
 go
 
 create function dbo.full_price_conference_attendees(@conference_id int)
 returns table
 as
-    return (select * from conference_attendees_details(@conference_id) as cad
+    return (select distinct * from conference_attendees_details(@conference_id) as cad
            where cad.is_student = 0)
 go
 
 create function dbo.full_price_conference_day_attendees(@conference_day_id int)
 returns table
 as
-    return (select * from conference_day_attendees_details(@conference_day_id) as cad
+    return (select distinct * from conference_day_attendees_details(@conference_day_id) as cad
            where cad.is_student = 0)
 go
 
 create function dbo.student_conference_day_attendees(@conference_day_id int)
 returns table
 as
-    return (select * from conference_day_attendees_details(@conference_day_id) as cad
+    return (select distinct * from conference_day_attendees_details(@conference_day_id) as cad
            where cad.is_student = 1)
 go
 
 create function dbo.workshops_during_conference_day(@conference_day_id int)
 returns table
 as
-    return (select w.workshop_id, w.workshop_title, w.workshop_description, w.attendees_workshop_max,
+    return (select distinct w.workshop_id, w.workshop_title, w.workshop_description, w.attendees_workshop_max,
                    w.price, w.start_time, w.end_time from workshops w
                    where w.conference_day_id = @conference_day_id)
 go
@@ -131,7 +131,7 @@ go
 create function dbo.reservations_booked_by_customer(@customer_id int)
 returns table
 as
-    return (select c.conference_id, c.name, cd.conference_day_id, cd.date,
+    return (select distinct c.conference_id, c.name, cd.conference_day_id, cd.date,
                    cdr.student_attendees, cdr.full_price_attendees, r.canceled
            from reservations r
            inner join conference_day_reservations cdr on r.reservation_id = cdr.reservation_id
@@ -143,7 +143,7 @@ go
 create function dbo.conference_days_of_registered(@registered_id int)
 returns table
 as
-    return (select c.conference_id, c.name, cd.conference_day_id,
+    return (select distinct c.conference_id, c.name, cd.conference_day_id,
                    cd.date, cda.is_student from registered r
            inner join conference_day_attendees cda on r.registered_id = cda.registered_id
            inner join conference_day_reservations cdr on cda.reservation_day_id = cdr.reservation_day_id
@@ -156,7 +156,7 @@ go
 create function dbo.workshops_of_registered(@registered_id int)
 returns table
 as
-    return (select w.workshop_id, w.workshop_title, w.workshop_description
+    return (select distinct w.workshop_id, w.workshop_title, w.workshop_description
            from workshops w
            inner join workshop_reservations wr on w.workshop_id = wr.workshop_id
            inner join conference_day_reservations cdr on wr.reservation_day_id = cdr.reservation_day_id
@@ -169,7 +169,7 @@ go
 create function dbo.conference_days_of_reservation(@reservation_id int)
 returns table
 as
-    return (select c.conference_id, c.name, cd.conference_day_id,
+    return (select distinct c.conference_id, c.name, cd.conference_day_id,
                    cd.date from reservations r
            inner join conference_day_reservations cdr on r.reservation_id = cdr.reservation_id
            inner join conference_days cd on cdr.conference_day_id = cd.conference_day_id

cf47ab4157a73eee882d42e76bd36bccbb2c95c7
Weronika Ormaniec <weronika.ormaniec@gmail.com>
2020-01-18 19:04:36 +0100

    more triggers

--- triggers.sql
+++ triggers.sql
@@ -16,6 +16,18 @@ if (object_id('dbo.enough_place_to_add_workshop_reservation') is not null)
 if (object_id('dbo.workshop_attendees_number_lower_than_max_attendees_number') is not null)
     drop trigger dbo.workshop_attendees_number_lower_than_max_attendees_number;
 
+if (object_id('dbo.price_levels_discount_decreases') is not null)
+    drop trigger dbo.price_levels_discount_decreases;
+
+if (object_id('dbo.reservation_date_before_conference_date') is not null)
+    drop trigger dbo.reservation_date_before_conference_date;
+
+if (object_id('dbo.price_level_dates_before_conference_date') is not null)
+    drop trigger dbo.price_level_dates_before_conference_date;
+
+if (object_id('dbo.no_attendees_at_two_workshops_at_the_same_time') is not null)
+    drop trigger dbo.no_attendees_at_two_workshops_at_the_same_time;
+
 
 create trigger dbo.conference_day_reservation_check_common_conference
     on conference_day_reservations
@@ -106,4 +118,71 @@ as
         end
     end
 go
+
+
+
+create trigger dbo.price_levels_discount_decreases
+    on price_levels
+    after insert, update
+as
+    begin
+        if exists
+            (select * from inserted
+             inner join price_levels as pl on pl.conference_id = inserted.conference_id
+             where (pl.date_from < inserted.date_from and pl.discount < inserted.discount)
+                    or
+                   (pl.date_from > inserted.date_from and pl.discount > inserted.discount))
+        begin throw 50001, 'Every next discount must be lower than the previous one', 1
+            rollback transaction
+        end
+    end
+go
+
+
+create trigger dbo.no_attendees_at_two_workshops_at_the_same_time
+    on workshop_attendees
+    after insert
+--w ramach tego samego dnia konferencji
+as
+    begin
+        if exists
+            (select * from inserted
+             inner join workshop_attendees as wa on wa.attendee_id = inserted.attendee_id and wa.reservation_workshop_id <> inserted.reservation_workshop_id
+             where dbo.two_reservations_for_workshops_at_the_same_time(wa.reservation_workshop_id, inserted.reservation_workshop_id) = 1)
+        begin throw 50001, 'Attendee cannot be registered for two workshops at the same time', 1
+            rollback transaction
+        end
+    end
+go
+
+create trigger dbo.reservation_date_before_conference_date
+    --sprawdzić
+    on reservations
+    after insert, update
+as
+    begin
+        if exists
+            (select * from inserted
+             inner join conference_day_reservations as cdr on inserted.reservation_id = cdr.reservation_id
+             inner join conference_days cd on cdr.conference_day_id = cd.conference_day_id
+             where cd.date <= inserted.reservation_date)
+        begin throw 50001, 'Reservation cannot be made after conference start', 1
+            rollback transaction
+        end
+    end
+
+create trigger dbo.price_level_dates_before_conference_date
+    --calls a view inside
+    on price_levels
+    after insert, update
+as
+    begin
+        if exists
+            (select * from inserted
+             inner join conferences_data as cd on cd.conference_id = inserted.conference_id
+             where cd.start_date < inserted.date_from)
+        begin throw 50001, 'Price level cannot start after conference start', 1
+            rollback transaction
+        end
+    end
 
